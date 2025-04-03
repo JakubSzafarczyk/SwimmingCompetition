ba2f94348079d2cb03bcd711ad9c5d6d8f8d789b
vnuczek <k.wnuk03@gmail.com>
2025-04-03 11:27:43 +0200

    Starting project

---
+++ SwimmingCompetition/src/main/java/com/polsl/demo/SwimmingCompetitionApplication.java
@@ -0,0 +1,13 @@
+package com.polsl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimmingCompetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimmingCompetitionApplication.class, args);
+	}
+
+}
---
+++ SwimmingCompetition/src/test/java/com/polsl/demo/SwimmingCompetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polsl.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimmingCompetitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
