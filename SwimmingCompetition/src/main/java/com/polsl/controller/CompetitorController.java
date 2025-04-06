82ddff2b107499c665ce651cd8e96c1059ac9b51
vnuczek <k.wnuk03@gmail.com>
2025-04-06 21:48:17 +0200

    adding entity models and controllers with basic management methods

--- SwimmingCompetition/src/main/java/com/polsl/controller/CompetitorController.java
+++ SwimmingCompetition/src/main/java/com/polsl/controller/CompetitorController.java
@@ -1,26 +1,52 @@
 package com.polsl.controller;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.polsl.entity.Competitor;
 import com.polsl.repository.CompetitorRepository;
 
-@Controller
-@RequestMapping("/competitor")
+@RestController
+@RequestMapping("/competitors")
 public class CompetitorController {
 
-	@Autowired
-	CompetitorRepository competitorRepo;
-	
-	@PostMapping
-	public @ResponseBody String addCompetitor(@RequestBody Competitor competitor) {
-		competitor = competitorRepo.save(competitor);
-		return "Added with id=" + competitor.getCompetitorId();
-	}
-	
-	@GetMapping
-	public @ResponseBody Iterable<Competitor> getCompetitor(@RequestParam String lastName) {
-	return competitorRepo.findByLastName(lastName);
-	}
+    @Autowired
+    private CompetitorRepository competitorRepository;
+    
+    @GetMapping
+    public Iterable<Competitor> getAll() {
+        return competitorRepository.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public Competitor getById(@PathVariable Long id) {
+        return competitorRepository.findById(id).orElse(null);
+    }
+    
+    @PostMapping
+    public Competitor create(@RequestBody Competitor competitor) {
+        return competitorRepository.save(competitor);
+    }
+    
+    @PutMapping("/{id}")
+    public Competitor update(@PathVariable Long id, @RequestBody Competitor competitorDetails) {
+        Optional<Competitor> optionalCompetitor = competitorRepository.findById(id);
+        if (optionalCompetitor.isPresent()) {
+            Competitor competitor = optionalCompetitor.get();
+            competitor.setFirstName(competitorDetails.getFirstName());
+            competitor.setSecondName(competitorDetails.getSecondName());
+            competitor.setLastName(competitorDetails.getLastName());
+            competitor.setDateOfBirth(competitorDetails.getDateOfBirth());
+            competitor.setGender(competitorDetails.getGender());
+            competitor.setNationality(competitorDetails.getNationality());
+            competitor.setTeam(competitorDetails.getTeam());
+            return competitorRepository.save(competitor);
+        }
+        return null;
+    }
+    
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        competitorRepository.deleteById(id);
+    }
 }
--- SwimmingCompetition/src/main/java/com/polsl/repository/CompetitorRepository.java
+++ SwimmingCompetition/src/main/java/com/polsl/repository/CompetitorRepository.java
@@ -1,13 +1,7 @@
 package com.polsl.repository;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
-
 import com.polsl.entity.Competitor;
 
-public interface CompetitorRepository extends CrudRepository<Competitor, Long>{
-	
-	List<Competitor> findByLastName(String lastName);
-
+public interface CompetitorRepository extends CrudRepository<Competitor, Long> {
 }
