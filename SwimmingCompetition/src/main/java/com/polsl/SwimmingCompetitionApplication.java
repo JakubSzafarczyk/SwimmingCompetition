77b95d19ed0b408cd403fa28a180042e4fff37fb
Jakub Szafarczyk <szafarczykjakub@gmail.com>
2025-04-07 19:21:00 +0200

    working database

--- SwimmingCompetition/src/main/java/com/polsl/SwimmingCompetitionApplication.java
+++ SwimmingCompetition/src/main/java/com/polsl/SwimmingCompetitionApplication.java
@@ -1,4 +1,4 @@
-package com.polsl.demo;
+package com.polsl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ SwimmingCompetition/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO competitors (
+    first_name,
+    second_name,
+    last_name,
+    date_of_birth,
+    gender,
+    nationality
+) VALUES (
+    'Jan',
+    NULL,
+    'Kowalski',
+    '1990-05-12',
+    'MALE',
+    'Polish'
+);
